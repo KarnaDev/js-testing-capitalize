876496fcbca07ad9ed3beb29d93f8562106866db
polina karna <polina.karna@gmail.com>
2024-03-17 21:07:45 +0300

    changed tests with assert modules

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -2,13 +2,20 @@
 // попробуйте изменять код функции / тестов, запуская проверки заново
 
 import capitalize from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
+
+// assert(capitalize('') === '');
+// assert(capitalize('hello') === 'Hello');
+
+assert.deepEqual(capitalize(''), '');
+assert.deepEqual(capitalize('hello'),'Hello');
 
 console.log('Все тесты пройдены!');
